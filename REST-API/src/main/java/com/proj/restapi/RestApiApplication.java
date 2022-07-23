758f096dbde44f95173843b26e89a2e9ab5276b4
May Tzadoki <mayta@mta.ac.il>
2022-07-23 09:54:33 +0300

    fixed - main

--- REST-API/src/main/java/com/proj/restapi/RestApiApplication.java
+++ REST-API/src/main/java/com/proj/restapi/RestApiApplication.java
@@ -1,20 +1,13 @@
 package com.proj.restapi;
 
-import java.util.List;
-
-import general.User;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestApiApplication implements CommandLineRunner {
+public class RestApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestApiApplication.class, args);
-    }
-    @Override
-    public void run(String... args) throws Exception {
     }
 
 }
