5ab15d8c1fcb36ff73a6eb42b4461e8d0b1eed45
movadia <may.ovadia@cognyte.com>
2022-08-01 18:50:40 +0300

    add report and alerts controllers

---
+++ REST-API/src/main/java/com/proj/restapi/alerts/controller/AlertsController.java
@@ -0,0 +1,21 @@
+package com.proj.restapi.alerts.controller;
+
+import com.proj.restapi.menu.service.SubscriberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class AlertsController {
+    @Autowired
+    private SubscriberService subscriberService;
+
+    @GetMapping("/alerts/{userId}")
+    public String alertPage(@PathVariable int userId, Model model){
+        model.addAttribute("subscriber", subscriberService.getSubscriberById(userId));
+
+        return "alertsPage";
+    }
+}
