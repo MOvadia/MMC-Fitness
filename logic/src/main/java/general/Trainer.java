b8261dc3c58110717bcfba6656e0c68192b5ef29
chenshitrit <chens@continuitysoftware.com>
2022-08-12 10:57:15 +0300

    Fixed the login with a trainer user

--- logic/src/main/java/general/Trainer.java
+++ logic/src/main/java/general/Trainer.java
@@ -1,12 +1,21 @@
 package general;
 
 public class Trainer extends User{
+
+    private enum specialty{};
+    private enum seniority{};
+
+    public Trainer() {
+    }
+
     public Trainer(Integer userId, String firstName, String lastName, String phoneNumber, String email) {
         super(userId, firstName, lastName, phoneNumber, email);
     }
 
-    private enum specialty{};
-    private enum seniority{};
+
+    public Trainer(User u, Trainer t) {
+        super(u.getUserId(),u.getFirstName(),u.getLastName(),u.getPhoneNumber(),u.getEmail());
+    }
 
 
 
