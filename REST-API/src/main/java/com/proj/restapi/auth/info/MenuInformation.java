f01bc4037087a5e225b036aaae0fd89cbbd82700
chenshitrit <chens@continuitysoftware.com>
2022-08-20 21:41:04 +0300

    no message

---
+++ REST-API/src/main/java/com/proj/restapi/auth/info/MenuInformation.java
@@ -0,0 +1,82 @@
+package com.proj.restapi.auth.info;
+
+public class WorkoutInformation {
+
+    private String workoutName;
+    private String link;
+    private Integer setNum;
+    private Integer repNum;
+    private String exerciseName;
+    private String description;
+
+    private String assignedUser;
+
+    public WorkoutInformation(String workoutName, String link, Integer setNum, Integer repNum, String exerciseName, String assignedUser, String description) {
+        this.workoutName = workoutName;
+        this.link = link;
+        this.setNum = setNum;
+        this.repNum = repNum;
+        this.exerciseName = exerciseName;
+        this.assignedUser = assignedUser;
+        this.description = description;
+    }
+
+    public WorkoutInformation() {
+    }
+
+    public String getWorkoutName() {
+        return workoutName;
+    }
+
+    public void setWorkoutName(String workoutName) {
+        this.workoutName = workoutName;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public Integer getRepNum() {
+        return repNum;
+    }
+
+    public void setRepNum(Integer repNum) {
+        this.repNum = repNum;
+    }
+
+    public String getExerciseName() {
+        return exerciseName;
+    }
+
+    public void setExerciseName(String exerciseName) {
+        this.exerciseName = exerciseName;
+    }
+
+    public void setSetNum(Integer setNum) {
+        this.setNum = setNum;
+    }
+
+    public Integer getSetNum() {
+        return setNum;
+    }
+
+    public String getAssignedUser() {
+        return assignedUser;
+    }
+
+    public void setAssignedUser(String assignedUser) {
+        this.assignedUser = assignedUser;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ REST-API/src/main/java/com/proj/restapi/nutritionist/controller/NutritionistController.java
@@ -0,0 +1,47 @@
+package com.proj.restapi.trainer.controller;
+
+import com.proj.restapi.auth.info.LoginInformation;
+import com.proj.restapi.auth.info.WorkoutInformation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.proj.restapi.trainer.service.NutritionistService;
+
+@Controller
+public class NutritionistController {
+    @Autowired
+    private NutritionistService trainerService = new NutritionistService();
+
+    @PostMapping(value = "/workout/add")
+    public void addWorkout(Model model, WorkoutInformation workoutInfo) {
+        model.addAttribute("workoutForm", workoutInfo);
+        trainerService.addWorkout(workoutInfo);
+
+
+    }
+
+    @DeleteMapping(value = "/{workoutId}")
+    public ResponseEntity<Object> deleteWorkout(@PathVariable int workoutId) {
+        return new ResponseEntity<>(trainerService.deleteWorkout(workoutId), HttpStatus.OK);
+    }
+
+    @GetMapping("/trainer/login/userId={userId}")
+    public String registerUser(@PathVariable int userId, Model model)
+    {
+        model.addAttribute("userForm", new LoginInformation());
+        //TODO - need to get user data from DB
+        //TODO - get from DB the userID - than get the trainer by UserId
+        int id = userId;
+        model.addAttribute("trainer", trainerService.getTrainerId(id));
+        model.addAttribute("workout", trainerService.getAllWorkouts());
+        model.addAttribute("gender", "Male");
+     //   model.addAttribute("workout", WorkoutService.getWorkoutPerUserId(1));
+        return "mainPageTrainer";
+    }
+}
