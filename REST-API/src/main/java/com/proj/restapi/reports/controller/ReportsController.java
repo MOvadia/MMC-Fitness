1c20b342d1c4d290f0fe2102b38e75ff16f3e06a
May Tzadoki <mayta@mta.ac.il>
2022-08-20 11:38:45 +0300

    pull data to google chart from the controller

--- REST-API/src/main/java/com/proj/restapi/reports/controller/ReportsController.java
+++ REST-API/src/main/java/com/proj/restapi/reports/controller/ReportsController.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import java.util.Map;
+import java.util.TreeMap;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ReportsController {
@@ -14,8 +18,17 @@ public class ReportsController {
     private SubscriberService subscriberService;
 
     @GetMapping("/report/userId={userId}")
-    public String alertPage(@PathVariable int userId, Model model){
+    public String reportPage(@PathVariable int userId, Model model){
         model.addAttribute("subscriber", subscriberService.getSubscriberById(userId));
+        Map<String, Double> graphData = new TreeMap<>();
+        //TODO: pull from the DB
+        graphData.put("week 1", 84.0);
+        graphData.put("week 2", 84.2);
+        graphData.put("week 3", 83.8);
+        graphData.put("week 4", 83.3);
+
+        model.addAttribute("chartData", graphData);
+
 
         return "reportsPage";
     }
