a7a4cdb9ed9e7f1991ecdd4351b098e2c1a46d5a
movadia <may.ovadia@cognyte.com>
2022-09-07 15:36:24 +0300

    add success msg to settings

--- REST-API/src/main/java/com/proj/restapi/settings/controller/SettingsController.java
+++ REST-API/src/main/java/com/proj/restapi/settings/controller/SettingsController.java
@@ -1,14 +1,10 @@
 package com.proj.restapi.settings.controller;
-
-import com.proj.restapi.auth.info.LoginInformation;
-import com.proj.restapi.auth.info.SubscriberInformation;
 import com.proj.restapi.menu.service.SubscriberService;
 import general.Subscriber;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -29,13 +25,28 @@ public class SettingsController {
     @PostMapping(value = "/settings/submit", params="cancel")
     public String registration(Subscriber user, Model model){
         int id = subscriberService.getUserIdByEmail(user.getEmail());
-        return "redirect:/menu/userId=" + id;
+        Subscriber subscriberInformation = subscriberService.getSubscriberById(id);
+        model.addAttribute("subscriber", subscriberInformation);
+        model.addAttribute("settingsForm", subscriberInformation);
+        model.addAttribute("resetMessage", "User data changes reset");
+        return "settings";
     }
 
     @PostMapping(value = "/settings/submit", params="submit")
     public String submit(Subscriber user, Model model){
+        int id = subscriberService.getUserIdByEmail(user.getEmail());
+
         int retVal = subscriberService.updateSubscriber(user);
-        return "redirect:/menu/userId=" + user.getUserId();
+        if (retVal !=0){
+            model.addAttribute("updateMessage", "User data changed");
+        }
+        else{
+            model.addAttribute("errorMessage", "User data failed to change");
+        }
+        Subscriber subscriberInformation = subscriberService.getSubscriberById(id);
+        model.addAttribute("subscriber", subscriberInformation);
+        model.addAttribute("settingsForm", subscriberInformation);
+        return "settings";
     }
 
 }
